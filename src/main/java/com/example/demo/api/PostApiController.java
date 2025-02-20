c4d4fd691c75c0925c8aeb60c73bcf5512255996
Jang-Je <jyhoo040912@gmail.com>
2025-02-21 00:19:40 +0900

    포스트 전체 조회 기능 추가

--- src/main/java/com/example/demo/api/PostApiController.java
+++ src/main/java/com/example/demo/api/PostApiController.java
@@ -11,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,25 +21,49 @@ public class PostApiController {
 
     private final PostService postService;
 
+    //포스트 전체 조회
+    @GetMapping("posts")
+    public List<SimplePostDTO> getPosts() {
+        List<Post> allPosts = postService.findAllPosts();
+        List<SimplePostDTO> result = allPosts.stream()
+                .map(o -> new SimplePostDTO(o))
+                .collect(toList());
+        return result;
+    }
+
+    @Data
+    public static class SimplePostDTO {
+        private String title;
+        private int price;
+
+        public SimplePostDTO(Post post) {
+            this.title = post.getTitle();
+            this.price = post.getPrice();
+        }
+    }
+
+    //포스트 상세 조회
     @GetMapping("posts/{postId}/")
     public PostDTO post(@PathVariable("postId") Long postId, Model model) {
         Post post = postService.findPostById(postId);
         return new PostDTO(post);
     }
 
-
+    //포스트 일부 수정
     @PutMapping("posts/{postId}")
     public PostDTO updatePost(@PathVariable("postId") Long postId, Model model) {
         Post post = postService.findPostById(postId);
         return new PostDTO(post);
     }
 
+    //포스트 전체 수정
     @PatchMapping("posts/{postId}")
     public PostDTO editPost(@PathVariable("postId") Long postId, Model model) {
         Post post = postService.findPostById(postId);
         return new PostDTO(post);
     }
 
+    //포스트 삭제
     @DeleteMapping("posts/{postId}")
     public void deletePost(@PathVariable("postId") Long postId, Model model) {
         Post post = postService.findPostById(postId);
@@ -44,7 +71,7 @@ public class PostApiController {
     }
 
     @Data
-    public class PostDTO {
+    public static class PostDTO {
         @Enumerated(EnumType.STRING)
         private PostType type;
 
