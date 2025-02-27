6d6a342604854c7d306282b45cfc99ae26467544
2heunxun <seapeon@naver.com>
2025-02-27 22:41:56 +0900

    Member CRUD Dev

--- src/main/java/repository/MemberRepository.java
+++ src/main/java/repository/MemberRepository.java
@@ -11,5 +11,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     void delete(Long memberId);
     Optional<Member> findByUserId(String userId);
-    Optional<Member> findByNickname(String nickname);
 }
