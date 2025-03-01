afb506287059b10e47afbde2da0f23b5fe405ee5
Jang-Je <jyhoo040912@gmail.com>
2025-03-01 20:08:43 +0900

    html 파일 업로드

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-@RequestMapping("/")
+@GetMapping("/")
     public String home() {
-    return "home";
+    return "mobile";
 }
+
+
 }
