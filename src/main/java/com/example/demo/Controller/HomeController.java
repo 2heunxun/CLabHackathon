17e71e9921ab2bf666092acb02ac9df602c57a5b
Jang-Je <jyhoo040912@gmail.com>
2025-03-12 18:20:14 +0900

    작성 시 post 생성하는 메소드 작성

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.MemberResponseDTO;
 import com.example.demo.domain.post.Post;
 import com.example.demo.dto.PostRequestDTO;
 import com.example.demo.dto.SimplePostDTO;
+import com.example.demo.service.ImageService;
 import com.example.demo.service.MemberService;
 import com.example.demo.service.PostService;
 import jakarta.servlet.http.HttpSession;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,6 +28,7 @@ public class HomeController {
 
     private final PostService postService;
     private final MemberService memberService;
+    private final ImageService imageService;
 
     @GetMapping
     public String getPostList(
@@ -59,8 +63,29 @@ public class HomeController {
 
 
     @PostMapping("/post")
-    public String createPost(@ModelAttribute PostRequestDTO postRequestDTO) {
-        return "redirect:/post";
+    public String createPost(HttpSession session, @ModelAttribute PostRequestDTO postRequestDTO) throws IOException {
+
+        Member author = (Member) session.getAttribute("loginMember");
+        // 로그인이 안 되어 있으면 로그인 페이지로 리다이렉트
+        if (author == null) {
+            return "redirect:/login";
+        }
+        Optional<String> image = imageService.uploadPostImage(postRequestDTO.getThumbnailImage());
+
+        Post post = Post.builder()
+                .type(postRequestDTO.getType())
+                .author(author)
+                .title(postRequestDTO.getTitle())
+                .content(postRequestDTO.getContent())
+                .price(postRequestDTO.getPrice())
+                .thumbnailUrl(image.orElse(null))
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .isDeleted(false)
+                .build();
+
+        Post savedPost = postService.save(post);
+        return "redirect:/mobile";
     }
 
 
